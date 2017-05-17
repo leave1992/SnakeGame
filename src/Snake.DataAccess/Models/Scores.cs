fb764e6c5a0c2f7c8996f1669656087bfe9937c7
VITALIK\1440 <1440@ukr.net>
2017-05-17 22:51:41 +0300

    Added .NET standard library  'Snake.DataAccess' and generated data context.

---
+++ src/Snake.DataAccess/Models/Scores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake.Game.Models
+{
+    public partial class Scores
+    {
+        public int ScoreId { get; set; }
+        public int Score { get; set; }
+        public DateTime? Date { get; set; }
+        public int? UserId { get; set; }
+
+        public virtual Users User { get; set; }
+    }
+}
